0256049138dfa3545f89f39cf01f572d02fd3d51
mikedilulo <mikedilulo2019@gmail.com>
2020-03-24 20:51:05 -0600

    import FormsModule for 2 way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
@@ -6,7 +7,7 @@ import { ServerComponent } from "./serverComponent/server.component";
 import { ServersComponent } from "./serversComponent/servers.component";
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
