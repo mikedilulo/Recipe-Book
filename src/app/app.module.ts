edb3518ac5da48e0016d55e4d09e1fe072dc3e40
mikedilulo <mikedilulo2019@gmail.com>
2020-03-24 18:54:35 -0600

    servers.comp added to NgModule & Import app.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./serverComponent/server.component";
+import { ServersComponent } from "./serversComponent/servers.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
