976fdf817b56b5f7d34438b256a6d0e8a86e67ff
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 20:49:09 -0600

    Add constructor ( ) and ngOnInit ( ) to RecDetail

--- src/app/recipeComponent/recipeDetailComponent/recipe-detail.component.ts
+++ src/app/recipeComponent/recipeDetailComponent/recipe-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-recipe-detail-component",
   templateUrl: "./recipe-detail.component.html",
   styleUrls: ["./recipe-detail.component.css"]
 })
-export class RecipeDetailComponent {}
+export class RecipeDetailComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
