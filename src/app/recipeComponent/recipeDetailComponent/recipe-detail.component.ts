1fa4d617a839625dc6cc0f188185c626e162b431
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:27:31 -0600

    Recipe Detail Component setup

---
+++ src/app/recipeComponent/recipeDetailComponent/recipe-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-detail-component",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
+})
+export class RecipeDetailComponent {}
