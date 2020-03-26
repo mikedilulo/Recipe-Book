20d4d5d6a05ed473cae68c1cf90cee37381d0b52
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:27:01 -0600

    Recipe Component setup

---
+++ src/app/recipeComponent/recipe.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-component",
+  templateUrl: "./recipe.component.html",
+  styleUrls: ["./recipe.component.css"]
+})
+export class RecipeComponent {}
