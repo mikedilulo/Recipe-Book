dda55125f752263a7cabab7a502d568fc436a20e
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:40:49 -0600

    RecipeComponent checked and import into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from "./headerComponent/header.component";
 import { RecipeItemComponent } from "./recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component";
 import { RecipeListComponent } from "./recipeComponent/recipeListComponent/recipe-list.component";
 import { RecipeDetailComponent } from "./recipeComponent/recipeDetailComponent/recipe-detail.component";
+import { RecipeComponent } from "./recipeComponent/recipe.component";
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipeItemComponent,
     RecipeListComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
