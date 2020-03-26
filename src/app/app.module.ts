ae58aeba966acae9a0d5578b6caa3713e2a2c6a7
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:37:34 -0600

    RecipeListComp checked and import into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./headerComponent/header.component";
 import { RecipeItemComponent } from "./recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component";
+import { RecipeListComponent } from "./recipeComponent/recipeListComponent/recipe-list.component";
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipeItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipeItemComponent,
+    RecipeListComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
