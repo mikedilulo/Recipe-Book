31db3920e933ba82d99b7e0cc722596e5f23f8d8
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:31:33 -0600

    Recipe Item Component setup

---
+++ src/app/recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-item-component",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.css"]
+})
+export class RecipeItemComponent {}
