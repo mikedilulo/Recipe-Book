e81c4f5ac7d4504b96c458d47e775d9d8ea93ee8
mikedilulo <mikedilulo2019@gmail.com>
2020-03-25 19:32:24 -0600

    Shopping List Edit Component setup

---
+++ src/app/ShoppingListComponent/ShoppingListEditComponent/shopping-list-edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-shopping-list-edit-component",
+  templateUrl: "./shopping-list-edit.component.html",
+  styleUrls: ["./shopping-list-edit.component.css"]
+})
+export class ShoppingListEditComponent {}
