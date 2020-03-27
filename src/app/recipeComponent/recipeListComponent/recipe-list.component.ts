8f803669874fcbbafeee2cde32a1e8995bc0f4a6
mikedilulo <mikedilulo2019@gmail.com>
2020-03-26 19:20:47 -0600

    Hard-coded recipe to render to page. Non dynamic

--- src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
+++ src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
@@ -7,7 +7,13 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Carmel Cake",
+      "Tasty treat for the whole family",
+      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=680&q=80"
+    )
+  ];
 
   constructor() {}
   ngOnInit() {}
