6c88ea5be6b38dc5b12876bdffe2248676109e17
mikedilulo <mikedilulo2019@gmail.com>
2020-03-26 19:41:11 -0600

    Added a second recipe and renders to the page

--- src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
+++ src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       "Carmel Cake",
       "Tasty treat for the whole family",
       "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=680&q=80"
+    ),
+    new Recipe(
+      "Candy Oreos",
+      "Sweet-Tooth Galore",
+      "https://images.unsplash.com/photo-1548848221-0c2e497ed557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
     )
   ];
 
