fd3d98f12b53d3cc27af899228a95f5f1e29e71d
mikedilulo <mikedilulo2019@gmail.com>
2020-03-26 20:27:47 -0600

    Hard coded Ingredient List renders to the page

--- src/app/ShoppingListComponent/shopping-list.component.ts
+++ src/app/ShoppingListComponent/shopping-list.component.ts
@@ -11,7 +11,8 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient("Skittles", 10),
     new Ingredient("Starbursts", 3),
     new Ingredient("Tootsie Rolls", 100),
-    new Ingredient("Chocolate Syrup", 5)
+    new Ingredient("Chocolate Syrup", 5),
+    new Ingredient("Oreo Cookies", 200)
   ];
 
   constructor() {}
