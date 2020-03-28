c4588fb30a5c638dca637bb5b02989bdf31482c6
mikedilulo <mikedilulo2019@gmail.com>
2020-03-28 17:01:23 -0600

    Added new EventEmitter to header.component.ts file

--- src/app/headerComponent/header.component.ts
+++ src/app/headerComponent/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-header-component",
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  featureSelected = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit() {}
