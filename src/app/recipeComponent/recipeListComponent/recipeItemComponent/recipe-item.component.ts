57453d36ef421022af3931ac1c1549559bf66a90
mikedilulo <mikedilulo2019@gmail.com>
2020-03-29 15:25:30 -0600

    Added @Output to listen from outside

--- src/app/recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component.ts
+++ src/app/recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../../recipe.model";
 
 @Component({
@@ -8,7 +8,7 @@ import { Recipe } from "../../recipe.model";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  recipeSelected = new EventEmitter<void>();
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() {}
   ngOnInit() {}
