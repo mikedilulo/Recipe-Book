a16f3d13733d01d37e4c6a030ea52b4ef1c4979c
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 15:08:39 -0600

    HC Ingredients to recipe service w/ recipes

--- src/app/recipeComponent/recipe.service.ts
+++ src/app/recipeComponent/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../sharedIngredientModel/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,24 @@ export class RecipeService {
     new Recipe(
       "Carmel Cake",
       "Tasty treat for the whole family",
-      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=680&q=80"
+      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=680&q=80",
+      [
+        new Ingredient("carmel", 5),
+        new Ingredient("cake batter", 2),
+        new Ingredient("chocolate", 4),
+        new Ingredient("butter", 2)
+      ]
     ),
     new Recipe(
       "Candy Oreos",
       "Sweet-Tooth Galore",
-      "https://images.unsplash.com/photo-1548848221-0c2e497ed557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+      "https://images.unsplash.com/photo-1548848221-0c2e497ed557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+      [
+        new Ingredient("oreos", 50),
+        new Ingredient("skittles", 14),
+        new Ingredient("starbursts", 25),
+        new Ingredient("oreo filling", 75)
+      ]
     )
   ];
 
