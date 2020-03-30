6c197ecc331573cb1699d7543381327c90d395a1
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 13:28:27 -0600

    Emits this.recipe of recipe item component

--- src/app/recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component.ts
+++ src/app/recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Recipe } from "../../recipe.model";
+import { RecipeService } from "../../recipe.service";
 
 @Component({
   selector: "app-recipe-item-component",
@@ -9,8 +10,10 @@ import { Recipe } from "../../recipe.model";
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
   ngOnInit() {}
 
-  onSelected() {}
+  onSelected() {
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
 }
