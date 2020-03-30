edae508754cc7c6735bb48c3f5d3093d911bdc56
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 12:58:55 -0600

    Removed HC recipe from recipe-list component file

--- src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
+++ src/app/recipeComponent/recipeListComponent/recipe-list.component.ts
@@ -8,18 +8,7 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      "Carmel Cake",
-      "Tasty treat for the whole family",
-      "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=680&q=80"
-    ),
-    new Recipe(
-      "Candy Oreos",
-      "Sweet-Tooth Galore",
-      "https://images.unsplash.com/photo-1548848221-0c2e497ed557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-    )
-  ];
+  recipes: Recipe[];
 
   constructor() {}
   ngOnInit() {}
