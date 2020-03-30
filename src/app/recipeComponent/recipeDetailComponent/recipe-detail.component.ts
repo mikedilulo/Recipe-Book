9721d5ebd167fd8d17a6becb4a014826e9cce1aa
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 15:23:35 -0600

    Injected Recipe Service / click listener on button

--- src/app/recipeComponent/recipeDetailComponent/recipe-detail.component.ts
+++ src/app/recipeComponent/recipeDetailComponent/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: "app-recipe-detail-component",
@@ -9,6 +10,8 @@ import { Recipe } from "../recipe.model";
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
   ngOnInit() {}
+
+  onAddToShoppingList() {}
 }
