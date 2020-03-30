cf42457ffd4ee49faa40652ab970d05d2883c5b5
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 13:53:39 -0600

    Utilized slService to add new ingredient

--- src/app/ShoppingListComponent/ShoppingListEditComponent/shopping-list-edit.component.ts
+++ src/app/ShoppingListComponent/ShoppingListEditComponent/shopping-list-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { Ingredient } from "src/app/sharedIngredientModel/ingredient.model";
+import { ShoppingListService } from "../shopping-list.service";
 
 @Component({
   selector: "app-shopping-list-edit-component",
@@ -10,7 +11,7 @@ export class ShoppingListEditComponent implements OnInit {
   @ViewChild("nameInput") nameInputRef: ElementRef;
   @ViewChild("amountInput") amountInputRef: ElementRef;
 
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {}
 
@@ -18,5 +19,6 @@ export class ShoppingListEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
   }
 }
