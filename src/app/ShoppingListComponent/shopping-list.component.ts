5f73914461f0425a466066b85b8fe3d2cedb77f6
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 13:42:52 -0600

    Removed HC ingredients / made private no access

--- src/app/ShoppingListComponent/shopping-list.component.ts
+++ src/app/ShoppingListComponent/shopping-list.component.ts
@@ -7,13 +7,7 @@ import { Ingredient } from "../sharedIngredientModel/ingredient.model";
   styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient("Skittles", 10),
-    new Ingredient("Starbursts", 3),
-    new Ingredient("Tootsie Rolls", 100),
-    new Ingredient("Chocolate Syrup", 5),
-    new Ingredient("Oreo Cookies", 200)
-  ];
+  private ingredients: Ingredient[];
 
   constructor() {}
   ngOnInit() {}
