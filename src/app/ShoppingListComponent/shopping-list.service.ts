58ad4becab547791ace91bb603268cc4ce8e7c34
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 13:41:23 -0600

    Copied Ingredients from ShoppingList.ts file

--- src/app/ShoppingListComponent/shopping-list.service.ts
+++ src/app/ShoppingListComponent/shopping-list.service.ts
@@ -1,1 +1,11 @@
-export class ShoppingListService {}
+import { Ingredient } from "../sharedIngredientModel/ingredient.model";
+
+export class ShoppingListService {
+  ingredients: Ingredient[] = [
+    new Ingredient("Skittles", 10),
+    new Ingredient("Starbursts", 3),
+    new Ingredient("Tootsie Rolls", 100),
+    new Ingredient("Chocolate Syrup", 5),
+    new Ingredient("Oreo Cookies", 200)
+  ];
+}
