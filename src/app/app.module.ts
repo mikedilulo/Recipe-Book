a6dcc6a42aa914463c399f2abff769bcb791b20a
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 12:58:11 -0600

    Added provider to recipe component child can use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from "./recipeComponent/recipeDetailComponent/r
 import { RecipeListComponent } from "./recipeComponent/recipeListComponent/recipe-list.component";
 import { RecipeItemComponent } from "./recipeComponent/recipeListComponent/recipeItemComponent/recipe-item.component";
 import { ShoppingListComponent } from "./shoppingListComponent/shopping-list.component";
-import {ShoppingListEditComponent} from "./shoppingListComponent/shoppingListEditComponent/shopping-list-edit.component"
+import { ShoppingListEditComponent } from "./shoppingListComponent/shoppingListEditComponent/shopping-list-edit.component";
 @NgModule({
   declarations: [
     AppComponent,
