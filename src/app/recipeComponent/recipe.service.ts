717c635f1e8de83ddbea0a6dfda03f8f399f904f
mikedilulo <mikedilulo2019@gmail.com>
2020-03-30 15:33:37 -0600

    can add ingredients to shopping list

--- src/app/recipeComponent/recipe.service.ts
+++ src/app/recipeComponent/recipe.service.ts
@@ -37,5 +37,7 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  addIngredientsToShoppingList(ingredients: Ingredient[]) {}
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
